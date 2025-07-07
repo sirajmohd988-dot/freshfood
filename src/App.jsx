e703b9b3c3780a15d475748755c228e929ad003d
sirajmohd988-dot <siraj.mohd@webseeder.tech>
2025-07-07 14:50:34 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "./components/header";
 import HeroSection from "./components/hero-section";
 import FeaturesSection from "./components/features-section";
 import SuggestedProducts from "./components/suggested-products";
